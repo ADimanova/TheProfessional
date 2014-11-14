feca7083dc42958721a790666f85eaacfa1c1fab
ADimanova <addi_91@abv.bg>
2014-11-15 00:02:46 +0200

    Added pages

---
+++ Professional.Web/Controllers/FieldController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Professional.Data;
+using Professional.Models;
+using Professional.Web.Models.DatabaseViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Professional.Web.Controllers
+{
+    public class FieldController : BaseController
+    {
+        public FieldController(IApplicationData data)
+            : base(data)
+        {
+
+        }
+        // GET: Field/Info
+        public ActionResult Info(string id)
+        {
+            var field = this.data.FieldsOfExpertise.All()
+                .FirstOrDefault(f => f.Name == id);
+
+            if (field == null)
+            {
+                return this.HttpNotFound("This field does not exist");
+            }
+
+            Mapper.CreateMap<FieldOfExpertise, FieldViewModel>();
+            var fieldInfoForView = Mapper.Map<FieldViewModel>(field);
+
+            return View(fieldInfoForView);
+        }
+    }
+}
---
+++ Professional.Web/Models/DatabaseViewModels/FieldViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Professional.Models;
+using Professional.Web.Infrastructure.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Professional.Web.Models.DatabaseViewModels
+{
+    public class FieldViewModel : IMapFrom<FieldOfExpertise>, IHaveCustomMappings
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+        public ICollection<string> Holders;
+
+        public void CreateMappings(AutoMapper.IConfiguration configuration)
+        {
+            configuration.CreateMap<FieldOfExpertise, FieldViewModel>()
+                .ForMember(p => p.Holders,
+                options => options.MapFrom(u => u.Holders.Select(h => h.FirstName + " " + h.LastName)));
+        }
+    }
+}
+
+ //       public int ID { get; set; }
+
+ //       [Required]
+ //       public string Name { get; set; }
+
+ //       [Required]
+ //       public int Rank { get; set; }
+
+ //       public virtual ICollection<User> Holders
+ //       {
+ //           get { return this.holders; }
+ //           set { this.holders = value; }
+ //       }
+
+ //       [Index]
+ //       public bool IsDeleted { get; set; }
+
+ //       public DateTime? DeletedOn { get; set; }
