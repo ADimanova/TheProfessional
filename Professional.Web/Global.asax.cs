a8c1a8e057cf1337ba1350cb17e8b0159fe3f342
ADimanova <addi_91@abv.bg>
2014-11-17 23:56:38 +0200

    AutoMapper as static class called in Global.asax; AutoMapper interfaces extracted in Professional.Contracts

--- Professional.Web/Global.asax.cs
+++ Professional.Web/Global.asax.cs
@@ -13,19 +13,12 @@ namespace Professional.Web
     {
         protected void Application_Start()
         {
-            //Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
-            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
-            //Thread.CurrentThread.CurrentCulture = CultureInfo.CurrentUICulture;
-
-            ViewEnginesConfig.SetEngines(ViewEngines.Engines);
-
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            var autoMapperConfig = new AutoMapperConfig();
-            autoMapperConfig.Execute();
+            ViewEnginesConfig.SetEngines(ViewEngines.Engines);
+            AutoMapperConfig.Execute();
         }
     }
 }
